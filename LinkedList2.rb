7dfe4976dc83d4f501c2b42ff27531cf73510b3f
Rajkumar <raj_muk@hotmail.com>
2019-08-14 17:17:48 +0000

    Completed Linked List #2 (Re-uploaded)

---
+++ LinkedList2.rb
@@ -0,0 +1,42 @@
+
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+# original function as set out in lesson
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+def reverse_list(list, previous=nil)
+	topper = list.next_node
+	list.next_node = previous
+	# recursion will reorder the nodes in reverse until value is nil
+	if topper != nil
+		reverse_list(topper, list)
+	else
+		list
+	end
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+print_values(node3)
+
+puts "-------"
+
+revlist = reverse_list(node3)
+print_values(revlist) 
